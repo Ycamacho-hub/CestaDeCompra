6795c0264642a2b7ea6797217b2f7999bdec8ca4
ycamacho <ycamacho@almata.cat>
2025-01-29 19:57:06 +0100

    aun mas

--- CestaDeCompra/Controllers/ProductoController.cs
+++ CestaDeCompra/Controllers/ProductoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CestaDeCompra.Models;
 using CestaDeCompra.Data;
+using System.Data;
 
 
 namespace CestaDeCompra.Controllers
@@ -14,5 +15,46 @@ namespace CestaDeCompra.Controllers
         {
             return View(prodRepo.GetProductos());
         }
+
+        [HttpGet]
+        public IActionResult Guardar(string codigo)
+        {
+
+            const string SessionKeyBuy = "_compra";
+            const string SessionKeyList = "_listaPrductos";
+
+            Producto p = new();
+            int numCompra = 0;
+
+            if(HttpContext.Session.GetInt32(SessionKeyBuy) == null)
+            {
+                numCompra = 0;
+                HttpContext.Session.SetInt32(SessionKeyBuy, numCompra+1);
+                //ViewData["compra"] = numCompra + 1;
+            }
+            else
+            {
+                numCompra = (int) HttpContext.Session.GetInt32(SessionKeyBuy);
+                HttpContext.Session.SetInt32(SessionKeyBuy, numCompra + 1);
+            }
+
+            if (HttpContext.Session.GetString(SessionKeyList) == null) 
+            {
+                    
+            }
+
+            ViewData["cod"] = codigo;
+
+
+             
+            return LocalRedirect ("/producto/index");
+        }
+
+        public string jsonProducto(Producto p)
+        {
+            return "{'cod': }";
+        }
+
+
     }
 }
