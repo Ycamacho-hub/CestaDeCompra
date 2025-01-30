44c917166f4b8e62e7e37da1c3e57369c342b6fd
Yostin Camacho <yosarca@gmail.com>
2025-01-30 11:27:16 +0100

    Almacenamiento de la cesta en la session

--- CestaDeCompra/Controllers/ProductoController.cs
+++ CestaDeCompra/Controllers/ProductoController.cs
@@ -2,6 +2,8 @@
 using CestaDeCompra.Models;
 using CestaDeCompra.Data;
 using System.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 
 namespace CestaDeCompra.Controllers
@@ -10,6 +12,8 @@ namespace CestaDeCompra.Controllers
     {
 
         public ProductoRepo prodRepo = new();
+        const string SessionKeyBuy = "_compra";
+        const string SessionKeyList = "_listaPrductos";
 
         public IActionResult Index()
         {
@@ -17,37 +21,52 @@ namespace CestaDeCompra.Controllers
         }
 
         [HttpGet]
-        public IActionResult Guardar(string codigo)
+        public IActionResult Guardar(string productCode)
         {
 
-            const string SessionKeyBuy = "_compra";
-            const string SessionKeyList = "_listaPrductos";
+            
 
             Producto p = new();
             int numCompra = 0;
+            List<Producto> listaProductos = [];
 
             if(HttpContext.Session.GetInt32(SessionKeyBuy) == null)
             {
                 numCompra = 0;
                 HttpContext.Session.SetInt32(SessionKeyBuy, numCompra+1);
                 //ViewData["compra"] = numCompra + 1;
+
+                listaProductos.Add(prodRepo.GetProducto(productCode));
+                string json = JsonSerializer.Serialize(listaProductos);
+                HttpContext.Session.SetString(SessionKeyList, json);
             }
             else
             {
                 numCompra = (int) HttpContext.Session.GetInt32(SessionKeyBuy);
                 HttpContext.Session.SetInt32(SessionKeyBuy, numCompra + 1);
+
+                string jsonP = HttpContext.Session.GetString(SessionKeyList);
+                listaProductos = JsonSerializer.Deserialize<List<Producto>>(jsonP);
+                listaProductos.Add(prodRepo.GetProducto(productCode));
+                jsonP = JsonSerializer.Serialize(listaProductos);
+                HttpContext.Session.SetString(SessionKeyList, jsonP);
             }
 
-            if (HttpContext.Session.GetString(SessionKeyList) == null) 
-            {
-                    
-            }
-
-            ViewData["cod"] = codigo;
-
-
-             
             return LocalRedirect ("/producto/index");
+        }
+
+
+        public IActionResult Cesta()
+        {
+            var listaProductos = new List<Producto>();
+
+            if(HttpContext.Session.GetString(SessionKeyList) != null)
+            {
+                string jsonP = HttpContext.Session.GetString(SessionKeyList);
+                listaProductos = JsonSerializer.Deserialize<List<Producto>>(jsonP);
+            }
+
+            return View(listaProductos);
         }
 
         public string jsonProducto(Producto p)
